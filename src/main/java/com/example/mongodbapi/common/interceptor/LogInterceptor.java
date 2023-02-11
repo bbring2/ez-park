c615b7e88df6f13a5262f344f690ccca2e6d5f28
bbring2 <jennachoi27@gmail.com>
2023-02-11 12:56:37 +0900

    added spring security dependency

--- src/main/java/com/example/mongodbapi/common/interceptor/LogInterceptor.java
+++ src/main/java/com/example/mongodbapi/common/interceptor/LogInterceptor.java
@@ -12,9 +12,6 @@ public class LogInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //When a specific method on controller layer is called by URI, preHandle method starts.
-        //this method would be called, before the controller called starts.
-        //we will manage this method to figure out the users who use this app about what they do on this app.
         log.debug("===============================================");
         log.debug("==================== BEGIN ====================");
         log.debug("Request URI ===> " + request.getRequestURI());
@@ -23,10 +20,6 @@ public class LogInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        /*
-        * 컨트롤러를 경유(접근) 한 후, 즉 화면(View)으로 결과를 전달하기 전에 실행되는 메서드입니다.
-        preHandle( )과는 반대로 요청(Request)의 끝을 알리는 로그가 콘솔에 출력되도록 처리합니다.
-        * */
         log.debug("==================== END ======================");
         log.debug("===============================================");
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
