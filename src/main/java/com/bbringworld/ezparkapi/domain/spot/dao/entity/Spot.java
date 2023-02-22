1f1ab0518bb418551b13628ccf4782ccfd051252
bbring <jennachoi27@gmail.com>
2023-02-22 16:27:57 +0900

    set up admin and spot responsibility

--- src/main/java/com/bbringworld/ezparkapi/domain/spot/dao/entity/Spot.java
+++ src/main/java/com/bbringworld/ezparkapi/domain/spot/dao/entity/Spot.java
@@ -1,4 +1,28 @@
 package com.bbringworld.ezparkapi.domain.spot.dao.entity;
 
-public class Spot {
+import com.bbringworld.ezparkapi.global.domain.dao.Registration;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "spot")
+public class Spot extends Registration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column(columnDefinition = "TINYINT(1)")
+    private boolean status;
+
 }
