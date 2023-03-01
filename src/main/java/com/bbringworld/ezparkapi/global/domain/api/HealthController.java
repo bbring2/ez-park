4994b903ab27f7942597956232a28feadaca1f83
bbring <jennachoi27@gmail.com>
2023-03-01 23:24:35 +0900

    changed health check

--- src/main/java/com/bbringworld/ezparkapi/global/domain/api/HealthController.java
+++ src/main/java/com/bbringworld/ezparkapi/global/domain/api/HealthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HealthController {
 
-    @GetMapping("/guest/auth")
+    @GetMapping
     public String hello() {
         return "This is health check tho :)";
     }
