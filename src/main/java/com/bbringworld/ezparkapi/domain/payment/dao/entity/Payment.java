be8a902aea1f3a6217ad5abbb812761236a27777
bbring <jennachoi27@gmail.com>
2023-03-06 16:45:53 +0900

    Develop (#18)
    * edited yml files
    
    * changed health check
    
    * added util classes
    
    * added test cases
    
    * added test cases
    
    * edited test cases
    
    * changed health check
    
    * added spot service layers
    
    * added apis for membership
    
    * added apis for membership
    
    * added spot service layers
    
    * set up querydsl for spring boot3
    
    * added querydsl
    
    * added jwt config

--- src/main/java/com/bbringworld/ezparkapi/domain/payment/dao/entity/Payment.java
+++ src/main/java/com/bbringworld/ezparkapi/domain/payment/dao/entity/Payment.java
@@ -1,4 +1,52 @@
 package com.bbringworld.ezparkapi.domain.payment.dao.entity;
 
-public class Payment {
+import com.bbringworld.ezparkapi.global.domain.dao.Registration;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@Entity
+@AllArgsConstructor
+@Table(name = "payment_log")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Payment extends Registration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "car_number")
+    private String carNumber;
+
+    @Column(name = "is_membership")
+    private boolean isMembership;
+
+    @Column
+    private boolean discounted;
+
+    @Column(name = "origin_amount")
+    private String originAmount;
+
+    @Column(name = "total_amount")
+    private String totalAmount;
+
+    @Column(name = "card_digit")
+    private String cardDigit;
+
+    @Column(name = "card_cmpny")
+    private String cardCmpny;
+
+    @Column
+    private String status;
+
 }
